3dde291c5e499ccd44889e4a420b32d61d6b8ab6
Patrick Brunner <brunner@stettbacher.ch>
2021-03-01 02:04:02 +0100

    Major cleanup

--- other_stuff/proj_stat_ng.py
+++ other_stuff/proj_stat_ng.py
@@ -16,7 +16,8 @@ datetime_or_null_pattern = '\'[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9
 string_pattern = '.*?'
 number_pattern = '[0-9]+'
 
-time_db = {
+db = {
+'time' : {
 	'pattern' : r'''
 		(?P<t_id>{number}),
 		(?P<t_proj>{number}),
@@ -33,9 +34,8 @@ time_db = {
 		date = date_pattern,
 		datetime = datetime_pattern),
 	'header' : ['Date', 'Employee_ID', 'Phase_ID', 'Project_ID', 'Time']
-}
-
-employee_db = {
+},
+'employee' : {
 	'pattern' : r'''
 		(?P<t_id>{number}),
 		'(?P<t_name>{string})',
@@ -52,9 +52,8 @@ employee_db = {
 		datetime = datetime_pattern),
 	'header' : ['Employee_ID', 'Username'],
 	'table' : []
-}
-
-project_db = {
+},
+'project' : {
 	'pattern' : r'''
 		(?P<t_id>{number}),
 		'(?P<t_name>{string})',
@@ -76,21 +75,21 @@ project_db = {
 		datetime_or_null = datetime_or_null_pattern),
 	'header' : ['Project_ID', 'Name', 'Description', 'Employee_ID'],
 	'table' : []
-}
+}}
 
 def debug_print(str):
 	if debug:
 		print(str)
 
 def get_employee_short_by_id(id):
-	for i in employee_db['table']:
-		if id == i[employee_db['header'].index('Employee_ID')]:
-			return i[employee_db['header'].index('Username')]
+	for i in db['employee']['table']:
+		if id == i[db['employee']['header'].index('Employee_ID')]:
+			return i[db['employee']['header'].index('Username')]
 
 def get_project_by_id(id):
-	for i in project_db['table']:
-		if id == i[project_db['header'].index('Project_ID')]:
-			return i[project_db['header'].index('Name')]
+	for i in db['project']['table']:
+		if id == i[db['project']['header'].index('Project_ID')]:
+			return i[db['project']['header'].index('Name')]
 
 
 def dump_monthly_table(monthly_table, user_list, use_days = False):
@@ -154,17 +153,17 @@ def analyse_project(project_id):
 	monthly_table = {}
 	user_list = []
 
-	for i,item in enumerate(time_db['table']):
+	for i,item in enumerate(db['time']['table']):
 
 		debug_print(item)
-		if item[time_db['header'].index('Project_ID')] != project_id:
+		if item[db['time']['header'].index('Project_ID')] != project_id:
 			continue
 
 		# times from table are in format e.g. '7.00\xa0h'
-		date_str = item[time_db['header'].index('Date')]
-		time = item[time_db['header'].index('Time')]
+		date_str = item[db['time']['header'].index('Date')]
+		time = item[db['time']['header'].index('Time')]
 
-		user = item[time_db['header'].index('Employee_ID')]
+		user = item[db['time']['header'].index('Employee_ID')]
 		if user not in user_list:
 			user_list.append(user)
 
@@ -215,19 +214,16 @@ def find_tables(dump_filename):
 		print('Tables:')
 		for line in f:
 			line_num += 1
-			line = line.decode('latin-1')
-			line = line.strip()
-			m = re.match('INSERT INTO `(.*)` VALUES (.*)', line)
-			if m:
-				table_title = m.group(1)
-				print('Line {}: {}'.format(line_num, table_title))
-				tables.append(table_title)
-
+			t,v = check_for_sql_table(line)
+			if t and v:
+				print('Line {}: {}'.format(line_num, t))
+				tables.append(t)
 	return tables
 
 # Check whether string is an SQL table. If yes, return a tuple of table title and value string. Otherwise return none.
 def check_for_sql_table(str):
-
+	str = str.decode('latin-1')
+	str = str.strip()
 	m = re.match('INSERT INTO `(.*)` VALUES (.*);', str)
 	if m and len(m.groups()) == 2:
 		return m.group(1), m.group(2)
@@ -235,88 +231,27 @@ def check_for_sql_table(str):
 		return None, None
 
 # Search dump for target_table, note that target_table might be split into several pieces
-def read_dump(dump_filename, target_table, transformation_cb):
+def read_dump(dump_filename, target_table):
 	table = []
 	line_num = 0
 	fast_forward = True
 	with open(dump_filename, 'rb') as f:
 		for line in f:
 			line_num += 1
-			line = line.decode('utf-8')
-			line = line.strip()
-
 			t,v = check_for_sql_table(line)
 			if t and v:
 				if t == target_table:
 					num = split_elements(v, table)
 					print('Found table {} at line {} with {} items'.format(target_table, line_num, num))
-	if transformation_cb:
-		return transformation_cb(table)
-	else:
-		return table
+	return transform_table(table, target_table)
 
-def transform_project_table(table):
-	out_table = []
-	num_items = 0
-	pattern = project_db['pattern']
-	header = project_db['header']
-	for item in table:
-		print(item)
-		m = re.match(pattern, item, re.VERBOSE)
-		if m:
-			entry = []
-			num_items += 1
-
-			for h_item in header:
-				if h_item == 'Project_ID':
-					entry.append(int(m.group('t_id')))
-				elif h_item == 'Employee_ID':
-					entry.append(int(m.group('t_employee')))
-				elif h_item == 'Name':
-					entry.append(m.group('t_name'))
-		else:
-			print('Failed to match entry')
-			print('|{}|'.format(item))
-			print('Pattern:')
-			print(pattern)
-			sys.exit(0)
-		out_table.append(entry)
-	return out_table
-
-def transform_employee_table(table):
-	out_table = []
-	num_items = 0
-	pattern = employee_db['pattern']
-	header = employee_db['header']
-	for item in table:
-		m = re.match(pattern, item, re.VERBOSE)
-		if m:
-			entry = []
-			num_items += 1
-
-			for h_item in header:
-				if h_item == 'Employee_ID':
-					entry.append(int(m.group('t_id')))
-				elif h_item == 'Username':
-					entry.append(m.group('t_login'))
-		else:
-			print('Failed to match entry')
-			print('|{}|'.format(item))
-			print('Pattern:')
-			print(pattern)
-			sys.exit(0)
-		out_table.append(entry)
-	return out_table
-
-def transform_time_table(table):
-
+def transform_table(table, table_type):
 	out_table = []
 	num_del_items = 0
 	num_items = 0
 	max_id = 0
-	pattern = time_db['pattern']
-	header = time_db['header']
-
+	pattern = db[table_type]['pattern']
+	header = db[table_type]['header']
 	for item in table:
 		m = re.match(pattern, item, re.VERBOSE)
 		if m:
@@ -331,27 +266,39 @@ def transform_time_table(table):
 				continue
 
 			for h_item in header:
-				if h_item == 'Project_ID':
-					entry.append(int(m.group('t_proj')))
-				elif h_item == 'Phase_ID':
-					entry.append(int(m.group('t_phase')))
-				elif h_item == 'Employee_ID':
-					entry.append(int(m.group('t_employee')))
-				elif h_item == 'Time':
-					h = int(m.group('t_hours'))
-					m = int(m.group('t_mins'))
-					t = h + m/60
-					entry.append(t)
-				elif h_item == 'Date':
-					entry.append(m.group('t_date'))
+
+				if table_type == 'project':
+					if h_item == 'Project_ID':
+						entry.append(int(m.group('t_id')))
+					elif h_item == 'Employee_ID':
+						entry.append(int(m.group('t_employee')))
+					elif h_item == 'Name':
+						entry.append(m.group('t_name'))
+				elif table_type == 'employee':
+					if h_item == 'Employee_ID':
+						entry.append(int(m.group('t_id')))
+					elif h_item == 'Username':
+						entry.append(m.group('t_login'))
+				elif table_type == 'time':
+					if h_item == 'Project_ID':
+						entry.append(int(m.group('t_proj')))
+					elif h_item == 'Phase_ID':
+						entry.append(int(m.group('t_phase')))
+					elif h_item == 'Employee_ID':
+						entry.append(int(m.group('t_employee')))
+					elif h_item == 'Time':
+						h = int(m.group('t_hours'))
+						m = int(m.group('t_mins'))
+						t = h + m/60
+						entry.append(t)
+					elif h_item == 'Date':
+						entry.append(m.group('t_date'))
 		else:
 			print('Failed to match entry')
 			print('|{}|'.format(item))
 			print('Pattern:')
 			print(pattern)
 			sys.exit(0)
-
-		debug_print(entry)
 		out_table.append(entry)
 
 	print('{}/{} deleted entries'.format(num_del_items, num_items))
@@ -390,18 +337,21 @@ def split_elements(str, table):
 
 	return num_elems_found
 
-def read_activity_table(infile):
-	table = read_dump(infile, "activity", None)
-	return table
+def read_sql_dump(infile):
 
-def read_time_table(infile):
-	time_db['table'] = read_dump(infile, "time", transform_time_table)
+	tables = find_tables(infile)
 
-def read_employee_table(infile):
-	employee_db['table'] = read_dump(infile, "employee", transform_employee_table)
+	for table_type in ['time', 'employee', 'project']:
+		if table_type in tables:
+			db[table_type]['table'] = read_dump(infile, table_type)
+		else:
+			print('Warning: table {} is missing'.format(table_type))
+			sys.exit(0)
 
-def read_project_table(infile):
-	project_db['table'] = read_dump(infile, "project", transform_project_table)
+	print('Number of items loaded:')
+	print('  Project: {}'.format(len(db['project']['table'])))
+	print('  Employee: {}'.format(len(db['employee']['table'])))
+	print('  Time: {}'.format(len(db['time']['table'])))
 
 if __name__ == '__main__':
 	if len(sys.argv) != 4:
@@ -411,16 +361,8 @@ if __name__ == '__main__':
 
 	infile = sys.argv[1]
 
-	print('\n===========================================\n')
-
-	tables = find_tables(infile)
-
-	read_activity_table(infile)
-	read_time_table(infile)
-	read_employee_table(infile)
-	read_project_table(infile)
+	read_sql_dump(infile)
 
 	if sys.argv[2] == '-p' and len(sys.argv) > 2:
 		proj_id = int(sys.argv[3])
 		analyse_project(proj_id)
-
