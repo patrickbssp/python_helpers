ac01cdbe164c5445797563d7a8360bdca63f6bf7
Patrick Brunner <brunner@stettbacher.ch>
2021-04-06 08:23:15 +0200

    New file

---
+++ MD5/concat_md5sums.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# rules:
+# a path to an MP3 file should have following structure:
+# Artist/Album/xx - Track.mp3
+# where xx is the track number
+# so, the directory hierarchy has 3 levels:
+# level 1: (Artist)
+#   - no files are allowed in this level, only directories
+# level 2: (Album)
+#   - no files are allowed in this level, only directories
+# level 3: (Track)
+#   - no directories are allowed in this level, only files
+#
+# rule_1a: no files are allowed in level 1
+# rule_2a: no files are allowed in level 2
+# rule_3a: no directories are allowed in level 3
+# rule_3b: filenames must have format 'xx - Trackname.mp3'
+
+import sys,re,os.path
+
+no_files = 0;
+no_mp3 = 0;
+no_violations = 0;
+
+with open('outfile.txt', 'w') as out_file:
+	for i in range(1,2):
+		gt = 'GT{:03}'.format(i)
+		filename = 'checksums/Checksums_CD_GT001-290/{}.md5'.format(gt)
+		print(filename)
+		with open(filename, 'r') as in_file:
+			for line in in_file:
+				pattern = re.compile("([0-9a-f]{32})(\s+\**)(.*)")
+				m = re.search(pattern, line)
+				if m:
+					m_hash = m.group(1)
+					m_file = m.group(3)
+				m_file = m_file.replace('/media/cdrom0', gt)
+				entry = '{}  {}\n'.format(m_hash, m_file)
+				out_file.write(entry)
+				print(entry)
+		
