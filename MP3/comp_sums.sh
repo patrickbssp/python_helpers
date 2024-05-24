086dd41d6c9a2f289af2e630e6d358ede4f0a4f7
Patrick Brunner <brunner@stettbacher.ch>
2024-05-24 13:26:31 +0200

    Simplified script

--- MP3/comp_sums.sh
+++ MP3/comp_sums.sh
@@ -1,2 +1,19 @@
-md5sum Take1/* > Take1.md5; md5sum Take2/* > Take2.md5; tkdiff Take1.md5 Take2.md5;sed s/Take2/Take1/ Take2.md5 > Take2_ed.md5;mv Take2_ed.md5 Take2.md5; tkdiff Take1.md5 Take2.md5
+#/bin/bash
+
+# Create checksum for MP3s and Waves and compare them
+
+# Take checksums of Take1, Take2, and wave files
+md5sum Take1/* > Take1.md5;
+md5sum Take2/* > Take2.md5;
 md5sum *.wav > wav.md5; more wav.md5
+
+# Compare Take1 and Take2 (should differ due to different folder names)
+diff Take1.md5 Take2.md5;
+echo "diff: $?"
+
+# Replace Take2 with Take1 in Take2.md5
+sed -i s/Take2/Take1/ Take2.md5
+
+# Compare Take1 and Take2 again (should match now)
+diff Take1.md5 Take2.md5
+echo "diff: $?"
