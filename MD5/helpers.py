2298ce171e9e405b5dc116e32b82b00003fa82aa
Patrick Brunner <brunner@stettbacher.ch>
2025-06-04 01:12:38 +0200

    Fixed globbing pattern, tests added

--- MD5/helpers.py
+++ MD5/helpers.py
@@ -4,23 +4,23 @@ import sys,re,os.path, chardet, glob
 
 # Return tuple consisting of hash and filename, or None
 def split_hash_filename(line):
-	pattern = re.compile("([0-9a-f]{32})(\s+\**)(.*)")
-	m = re.search(pattern, line)
-	if m:
-		d = dict()
-		d['hash'] = m.group(1)
-		d['ws'] = m.group(2)
-		d['file'] = m.group(3)
-		return d
+    pattern = re.compile("([0-9a-f]{32})(\s+\**)(.*)")
+    m = re.search(pattern, line)
+    if m:
+        d = dict()
+        d['hash'] = m.group(1)
+        d['ws'] = m.group(2)
+        d['file'] = m.group(3)
+        return d
 
 def open_and_decode_file(in_fname):
 
-	with open(in_fname, 'rb', encoding=None) as in_f:
-		raw_data = in_f.read()
-		det = chardet.detect(raw_data)
-		print('File {}, encoding: {} (confidence: {})'.format(in_fname, det['encoding'], det['confidence']))
-		data = raw_data.decode(det['encoding'])
-		return data
+    with open(in_fname, 'rb', encoding=None) as in_f:
+        raw_data = in_f.read()
+        det = chardet.detect(raw_data)
+        print('File {}, encoding: {} (confidence: {})'.format(in_fname, det['encoding'], det['confidence']))
+        data = raw_data.decode(det['encoding'])
+        return data
 
 # Create a filelist from a string containing filenames or foldernames. Filenames are added as they are.
 # Folders are searched for files, depending on the recursive flag, subfolders are searched as well, or ignored.
@@ -28,30 +28,40 @@ def open_and_decode_file(in_fname):
 # is the list of files/folders as described above.
 # Note that nargs must be used with the path argument to accept multiple arguments.
 # e.g.:
-#	parser.add_argument('path', nargs='+', help='Folder containing files, or single/multiple file(s)')
+#   parser.add_argument('path', nargs='+', help='Folder containing files, or single/multiple file(s)')
 
-def create_filelist(args, file_ext=None):
-	'''args is a list or single item containing files and/or paths.
-	file_ext is a string specifiying the allowed file extensions.
-	'''
-	filelist = []
-	for path in args:
-		if os.path.isdir(path):
-			print('Got path: ', path)
-			# Path is a directory. Collect all files and add them
-			# TODO if file_ext is None, don't filter files
-			if file_ext:
-				glob_patt = '{}/*.{}'.format(path, file_ext)
-			else:
-				glob_patt = '{}/*'.format(path)
-			filelist = glob.glob(glob_patt)
-		elif os.path.isfile(path):
-			print('Got file: ', path)
-			# Path is a single file
-			filelist.append(path)
-		else:
-			print('Invalid path: {}'.format(path))
+def create_filelist(args, file_ext=None, recursive=False):
+    '''args is a list or single item containing files and/or paths.
+    file_ext is a string specifiying the allowed file extensions.
+    '''
+    # Using set to prevent duplicates
+    filelist = []
+    for path in args:
+        if os.path.isdir(path):
+            print('Got path: ', path)
+            # Path is a directory. Collect all files and add them
+            # TODO if file_ext is None, don't filter files
+            if file_ext:
+                glob_patt = '{}/*.{}'.format(path, file_ext)
+            else:
+                if recursive:
+                    glob_patt = '**/*'
+                else:
+                    glob_patt = '*'
+            res = glob.glob(glob_patt, root_dir=path, recursive=recursive)
+            for r in res:
+                f = os.path.join(path, r)
+                if os.path.isfile(f):
+                    filelist.append(f)
 
-	return filelist
+        elif os.path.isfile(path):
+            print('Got file: ', path)
+            # Path is a single file
+            filelist.append(path)
+        else:
+            print('Invalid path: {}'.format(path))
+
+    # Convert to set and back to list to get rid of duplicates
+    return list(set(filelist))
 
 
--- MD5/test_create_filelist.py
+++ MD5/test_create_filelist.py
@@ -4,26 +4,47 @@ from pyfakefs.fake_filesystem_unittest import TestCase
 import helpers 
 
 class MyTest(TestCase):
-	def setUp(self):
-		self.setUpPyfakefs()
+    def setUp(self):
+        self.setUpPyfakefs()
 
-	def test_with_fake_file(self):
+    def test_with_fake_file(self):
         
-		# create fake files
-		self.fs.create_file('./DirA/file1.txt')
-		self.fs.create_file('./DirA/file2.txt')
-		self.fs.create_file('./DirA/file3.wav')
-		self.fs.create_file('./DirA/DirAA/file1.txt')
-		self.fs.create_file('./DirB/file1.mp3')
-		self.fs.create_file('./DirB/file2.txt')
-		self.fs.create_file('./DirB/file3.txt')
-		self.fs.create_file('./DirB/DirBA/file1.txt')
-		self.fs.create_file('./file1.txt')
-		self.fs.create_file('./file2.mp3')
-		self.fs.create_file('./file3.txt')
+        # create fake files
+        self.fs.create_file('./DirA/file1.txt')
+        self.fs.create_file('./DirA/file2.txt')
+        self.fs.create_file('./DirA/file3.wav')
+        self.fs.create_file('./DirA/DirAA/file1.txt')
+        self.fs.create_file('./DirB/file1.mp3')
+        self.fs.create_file('./DirB/file2.txt')
+        self.fs.create_file('./DirB/file3.txt')
+        self.fs.create_file('./DirB/DirBA/file1.txt')
+        self.fs.create_file('./file1.txt')
+        self.fs.create_file('./file2.mp3')
+        self.fs.create_file('./file3.txt')
+        print(self.fs)
 
-		fl = helpers.create_filelist("./DirA/file1.txt ./DirA/file2.txt")
-		self.assertEqual(fl, '')
-		print(fl)
+        # Two files
+        fl = helpers.create_filelist(["./DirA/file1.txt", "./DirA/file2.txt"])
+        exp_list = ["./DirA/file1.txt", "./DirA/file2.txt"]
+        self.assertEqual(sorted(fl), sorted(exp_list))
 
-#		self.assertEqual(content, 'hello wonnrld')
+        # All files in current folder only
+        fl = helpers.create_filelist(["."])
+        exp_list = ["./file1.txt", "./file2.mp3", "./file3.txt"]
+        self.assertEqual(sorted(fl), sorted(exp_list))
+
+       # All files recursively
+        fl = helpers.create_filelist(["."], recursive=True)
+        exp_list = [
+            './DirA/file1.txt', './DirA/file2.txt', './DirA/file3.wav', './DirA/DirAA/file1.txt',
+            './DirB/file1.mp3', './DirB/file2.txt', './DirB/file3.txt', './DirB/DirBA/file1.txt',
+            "./file1.txt", "./file2.mp3", "./file3.txt"]
+        self.assertEqual(sorted(fl), sorted(exp_list))
+
+        # All files recursively, plus another file
+        fl = helpers.create_filelist([".", "./DirA/file1.txt"], recursive=True)
+        exp_list = [
+            './DirA/file1.txt', './DirA/file2.txt', './DirA/file3.wav', './DirA/DirAA/file1.txt',
+            './DirB/file1.mp3', './DirB/file2.txt', './DirB/file3.txt', './DirB/DirBA/file1.txt',
+            "./file1.txt", "./file2.mp3", "./file3.txt"]
+        self.assertEqual(sorted(fl), sorted(exp_list))
