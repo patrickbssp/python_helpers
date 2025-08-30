2abebfa10e4eaf6e960924934ae2047095768631
Patrick Brunner <brunner@stettbacher.ch>
2025-08-31 01:02:02 +0200

    Scripts extended

--- MP3/count_vorbis_tags.py
+++ MP3/count_vorbis_tags.py
@@ -7,7 +7,23 @@ if __name__ == "__main__":
     dir_name = sys.argv[1]
     empty_tags = set()
     for filename in glob.glob(dir_name+'/**/*.flac', recursive=True):
+        path_rel = pathlib.PurePath(filename)
+        if len(part_rel.parts) != 3:
+            print(f'Invalid filestructure: {filename}')
         f = FLAC(filename)
+        missing_tags = []
+        expected_tags = ('artist', 'album', 'genre', 'title', 'tracknumber', 'date')
+        for k in expected_tags:
+            if not k in f:
+                missing_tags.append(k)
+        if len(missing_tags):
+            print(f'{filename:60}\nMissing tags: ', end='')
+            for i, tag in enumerate(missing_tags):
+                print(f'{tag}', end='')
+                if i < len(missing_tags)-1:
+                    print(',', end='')
+                else:
+                    print()
         if len(f) == 0:
             path = os.path.dirname(filename)
             empty_tags.add(path)
