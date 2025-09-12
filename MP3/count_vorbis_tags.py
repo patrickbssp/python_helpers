f0719808c842cb13365d0c812757f8f18019c8ab
Patrick Brunner <brunner@stettbacher.ch>
2025-09-12 19:19:44 +0200

    Extended functionality of count_vorbis_tags.py

--- MP3/count_vorbis_tags.py
+++ MP3/count_vorbis_tags.py
@@ -1,31 +1,80 @@
 #!/usr/bin/env python3
 
 import os, sys, glob
-from mutagen.flac import FLAC
+import pathlib
+import math
 
-if __name__ == "__main__":
-    dir_name = sys.argv[1]
+# custom modules
+mod_path = pathlib.Path(__file__).resolve().parents[1]/'helpers'
+sys.path.insert(0, str(mod_path))
+import helpers, extract_tags
+
+def collect_file_info(dir_name):
+    file_info = []
+    for filename in glob.glob(dir_name+'/**/*.*', recursive=True):
+        _,ext = os.path.splitext(filename)
+        if ext in ['.mp3', '.flac']:
+            f = extract_tags.FileInfo(filename)
+            file_info.append(f)
+    return sorted(file_info)
+
+def dump_all_tags(file_info):
+    expected_tags = ['t_artist', 't_album', 't_track', 't_title', 't_genre', 't_date']
+    # Determine field lengths
+    f_len = {}
+    for e in expected_tags:
+        f_len[e] = 0
+    for f in file_info:
+        for i, tag in enumerate(expected_tags):
+            f_len[tag] = max(f_len[tag], len(f[tag]))
+    print(f_len)
+    for f in file_info:
+        for i, tag in enumerate(expected_tags):
+            print(f'{f[tag]:{f_len[tag]}}', end='')
+            if i < len(expected_tags)-1:
+                print(',', end='')
+            else:
+                print()
+    
+def dump_missing_tags(file_info):
     empty_tags = set()
-    for filename in glob.glob(dir_name+'/**/*.flac', recursive=True):
-        path_rel = pathlib.PurePath(filename)
-        if len(part_rel.parts) != 3:
-            print(f'Invalid filestructure: {filename}')
-        f = FLAC(filename)
+    print('Files with partially or fully empty tags:')
+    for f in file_info:
         missing_tags = []
-        expected_tags = ('artist', 'album', 'genre', 'title', 'tracknumber', 'date')
+        expected_tags = ['t_artist', 't_album', 't_genre', 't_title', 't_track', 't_date']
         for k in expected_tags:
-            if not k in f:
+            if not k in f.get_tags() or f[k] == '':
                 missing_tags.append(k)
         if len(missing_tags):
-            print(f'{filename:60}\nMissing tags: ', end='')
+            print(f'{f.file_name:60}\nMissing tags: ', end='')
             for i, tag in enumerate(missing_tags):
                 print(f'{tag}', end='')
                 if i < len(missing_tags)-1:
                     print(',', end='')
                 else:
                     print()
-        if len(f) == 0:
-            path = os.path.dirname(filename)
+        # Collect files where all tags of interest are missing
+        if missing_tags == expected_tags:
+            path = os.path.dirname(f.file_name)
             empty_tags.add(path)
+    print('Folders containing files with fully empty tags:')
     for item in sorted(empty_tags):
         print(item)
+
+def print_usage_and_die():
+    print("""Usage:
+    -d <top_dir>     Dump all tags in given directory.
+    -m <top_dir>     Dump missing tags for files in given directory.
+    """)
+    sys.exit(1)
+    
+if __name__ == "__main__":
+    # Require at least one argument
+    if len(sys.argv) < 3:
+        print_usage_and_die()
+    if sys.argv[1] == '-d' and len(sys.argv) == 3:
+        file_info = collect_file_info(sys.argv[2])
+        dump_all_tags(file_info)
+    if sys.argv[1] == '-m' and len(sys.argv) == 3:
+        file_info = collect_file_info(sys.argv[2])
+        dump_missing_tags(file_info)
